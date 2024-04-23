51fdcd32a0e3cd97f136b7b1561406288a9ce002
Byron G. Soriano <byronreb100@gmail.com>
2024-04-23 13:02:49 -1000

    new files

---
+++ app/imports/ui/pages/AddUserInfo.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { Users } from '/imports/api/users/Users'; // Adjust import path
+
+// Create a schema to specify the structure of the data to appear in the form.
+const formSchema = new SimpleSchema({
+  username: String,
+  email: String,
+  profilePicture: {
+    type: String,
+    optional: true,
+  },
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+/* Renders the AddStuff page for adding a document. */
+const AddUser = () => {
+  // On submit, insert the data.
+  const submit = (data, formRef) => {
+    const { username, email } = data;
+    const owner = Meteor.user().username;
+    Users.collection.insert(
+      { username, email, owner },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Information added successfully', 'success');
+          formRef.reset();
+        }
+      },
+    );
+  };
+
+  // Render the form.
+  let fRef = null;
+  return (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={5}>
+          <Col className="text-center"><h2>Add User</h2></Col>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
+            <Card>
+              <Card.Body>
+                <TextField name="username" />
+                <TextField name="email" />
+                <SubmitField value="Submit" />
+                <ErrorsField />
+              </Card.Body>
+            </Card>
+          </AutoForm>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default AddUser;
