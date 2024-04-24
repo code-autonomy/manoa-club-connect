28892aa8cb22d8fed44d97c2b87e05f7e203e133
jhasun <jhasun@hawaii.edu>
2024-04-23 19:58:51 -1000

    Exporting mup.js and settings.json

--- app/.deploy/mup.js
+++ app/.deploy/mup.js
@@ -1,20 +1,20 @@
 module.exports = {
   servers: {
     one: {
-      host: '111.222.333.444',
+      host: 'manoa-club-connect.xyz',
       username: 'root',
-      password: 'changeme'
+      password: 'v_A?4-sYeV^-eZu'
     }
   },
   app: {
     // if you edit the app 'name' field, be sure to run 'mup stop' if the app is already running.
     // otherwise you will have two apps deployed at once, with unpredictable results.
-    name: 'meteor-react-bootstrap-template',
+    name: 'Manoa-Club-Connect',
     path: '../',
     servers: { one: {}, },
     buildOptions: { serverOnly: true },
     env: {
-      ROOT_URL: 'http://111.222.333.444',
+      ROOT_URL: 'https://manoa-club-connect.xyz',
       MONGO_URL: 'mongodb://mongodb/meteor',
       MONGO_OPLOG_URL: 'mongodb://mongodb/local',
     },
@@ -22,5 +22,12 @@ module.exports = {
     enableUploadProgressBar: true
   },
   mongo: { version: '5.0', servers: { one: {} }
+  },
+  proxy: {
+    domains: 'manoa-club-connect.xyz',
+    ssl: {
+      letsEncryptEmail: 'jhasun@hawaii.edu',
+      forceSSL: true
+    },
   },
 };
