78cd5a5e66acf2b5f53ca20f7b2ff00a2fb83ffd
Cristian Milanes <cmilanes@hawaii.edu>
2024-04-24 15:18:06 -1000

    Working Implementation of CC page

--- app/imports/ui/components/ClubCards.jsx
+++ app/imports/ui/components/ClubCards.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 
 const ClubCards = ({ clubs }) => (
   <div>
-    {/* eslint-disable-next-line react/prop-types */}
     {clubs.map((club) => (
       <ClubCard key={club._id} club={club} />
     ))}
--- app/imports/ui/pages/ClubCategoriesPage.jsx
+++ app/imports/ui/pages/ClubCategoriesPage.jsx
@@ -1,39 +1,70 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import PropTypes from 'prop-types'; // Import PropTypes
-import { withTracker } from 'meteor/react-meteor-data';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Container, Row, Col, Carousel } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { Clubs } from '../../api/club/Club';
-import ClubOrganizer from '../components/ClubOrganizer';
+import { ClubCards } from '../components/ClubCards';
+import LoadingSpinner from '../components/LoadingSpinner';
 
-const ClubsPage = ({ clubs }) => (
-  <div>
-    <h1>All Clubs</h1>
-    <ClubOrganizer clubs={clubs} />
-  </div>
-);
+const ClubCategoriesPage = () => {
+  const { clubs, ready } = useTracker(() => {
+    const subscription = Meteor.subscribe('clubs');
+    const rdy = subscription.ready();
+    const items = Clubs.collection.find({}).fetch();
+    return {
+      clubs: items,
+      ready: rdy,
+    };
+  });
 
-// Define PropTypes for ClubsPage component
-ClubsPage.propTypes = {
-  clubs: PropTypes.arrayOf(
-    PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      organization: PropTypes.string.isRequired,
-      dateApproved: PropTypes.string.isRequired,
-      expiration: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired,
-      email: PropTypes.string.isRequired,
-      purpose: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
+  // Function to group clubs by category
+  const groupClubsByCategory = (clubsGroup) => {
+    const groupedClubs = {};
+    clubsGroup.forEach((club) => {
+      const { type } = club;
+      if (!groupedClubs[type]) {
+        groupedClubs[type] = [];
+      }
+      groupedClubs[type].push(club);
+    });
+    return groupedClubs;
+  };
+
+  return ready ? (
+    <Container fluid className="ps-3 mb-5">
+      <Row className="justify-content-center">
+        <Col md={10}>
+          <Row className="pt-3 mb-3">
+            <Col />
+            <Col className="text-justify">
+              <h2>Current Active Clubs</h2>
+            </Col>
+          </Row>
+          {/* Group clubs by category and render a carousel for each category */}
+          {Object.entries(groupClubsByCategory(clubs)).map(([category, clubsInCategory]) => (
+            <div key={category} className="mb-4">
+              <h3 className="mb-3">{category}</h3>
+              <div style={{ overflow: 'hidden' }}> {/* Add overflow hidden to contain carousel */}
+                <Carousel
+                  nextIcon={<span className="carousel-arrow">›</span>}
+                  prevIcon={<span className="carousel-arrow">‹</span>}
+                  nextLabel=""
+                  prevLabel="" // Hide default aria-labels
+                >
+                  {clubsInCategory.map((club) => (
+                    <Carousel.Item key={club._id}>
+                      <ClubCards clubs={[club]} />
+                    </Carousel.Item>
+                  ))}
+                </Carousel>
+              </div>
+            </div>
+          ))}
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />;
 };
 
-// Fetch clubs data from the database
-const ClubsCategoriesPage = withTracker(() => {
-  Meteor.subscribe('clubs'); // Subscribe to the 'clubs' publication
-
-  return {
-    clubs: Clubs.collection.find().fetch(),
-  };
-})(ClubsPage);
-
-export default ClubsCategoriesPage;
+export default ClubCategoriesPage;
