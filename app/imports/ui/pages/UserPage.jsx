9b3e691f262bfed0addf309434a208221cdeab02
Byron G. Soriano <byronreb100@gmail.com>
2024-05-07 20:26:40 -1000

    deleted defaults

--- app/imports/ui/pages/UserPage.jsx
+++ app/imports/ui/pages/UserPage.jsx
@@ -28,7 +28,6 @@ const UserPage = () => {
   const userEmail = currentUser ? currentUser.email : '';
   console.log(userEmail);
   const currentUserFirstName = currentUser ? currentUser.firstName : '';
-  const isVendor = currentUser && currentUser.title === 'Vendor';
 
   return (ready ? (
     <Container fluid id="view-user-page" className="min-vh-100">
@@ -47,14 +46,13 @@ const UserPage = () => {
           </Col>
         </Col>
         <Col md={6}>
-          {isVendor && (
-            <Col fluid className="text-center py-5">
-              <h2>Want To Add A New Restaurant?</h2>
-              <Button size="lg" block className="text-center mt-3 custom-review-button" onClick={goAddRestaurant}>
-                Add A New Restaurant
-              </Button>
-            </Col>
-          )}
+          <Col fluid className="text-center py-5">
+            <h2>Want To Add A New Restaurant?</h2>
+            <Button size="lg" block className="text-center mt-3 custom-review-button" onClick={goAddRestaurant}>
+              Add A New Restaurant
+            </Button>
+          </Col>
+          )
           <Col className="text-center py-4">
             <h2>Clubs</h2>
             <Button size="lg" block className="text-center mt-3 custom-review-button" onClick={goToLeaveReview}>
